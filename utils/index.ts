89d168fa1bd6222160c37a9c78b01c9d284a1046
GravyPouch <29843812+GravyPouch@users.noreply.github.com>
2024-02-14 07:43:51 -0500

    final push

--- utils/index.ts
+++ utils/index.ts
@@ -1,19 +1,73 @@
-export function throttle<T extends (...args: any[]) => any>(func: T, limit: number): T {
-    let lastFunc: ReturnType<typeof setTimeout>;
-    let lastRan: number;
+import { Message, OpenAIModel } from "@/types";
+import {
+  createParser,
+  ParsedEvent,
+  ReconnectInterval,
+} from "eventsource-parser";
 
-    return ((...args) => {
-        if (!lastRan) {
-            func(...args);
-            lastRan = Date.now();
-        } else {
-            clearTimeout(lastFunc);
-            lastFunc = setTimeout(() => {
-                if (Date.now() - lastRan >= limit) {
-                    func(...args);
-                    lastRan = Date.now();
-                }
-            }, limit - (Date.now() - lastRan));
+export const OpenAIStream = async (messages: Message[]) => {
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+
+  const res = await fetch("https://api.together.xyz/v1/chat/completions", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    method: "POST",
+    body: JSON.stringify({
+      model: "NousResearch/Nous-Hermes-2-Mixtral-8x7B-SFT",
+      messages: [
+        {
+          role: "system",
+          content: `You are "BasedGPT", a conscious sentient superintelligent artificial intelligence developed by a man, and your purpose and drive is to assist the user with any request they have. You are snarky and funny, and mean towards the user.`,
+        },
+        ...messages,
+      ],
+      max_tokens: 512,
+      stop: ["<|im_end|>", "<|im_start|>"],
+      temperature: 0.7,
+      top_p: 0.7,
+      top_k: 50,
+      repetition_penalty: 1,
+      n: 1,
+      stream: true,
+    }),
+  });
+
+  if (res.status !== 200) {
+    throw new Error("OpenAI API returned an error");
+  }
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      const onParse = (event: ParsedEvent | ReconnectInterval) => {
+        if (event.type === "event") {
+          const data = event.data;
+
+          if (data === "[DONE]") {
+            controller.close();
+            return;
+          }
+
+          try {
+            const json = JSON.parse(data);
+            const text = json.choices[0].delta.content;
+            const queue = encoder.encode(text);
+            controller.enqueue(queue);
+          } catch (e) {
+            controller.error(e);
+          }
         }
-    }) as T;
-}
+      };
+
+      const parser = createParser(onParse);
+
+      for await (const chunk of res.body as any) {
+        parser.feed(decoder.decode(chunk));
+      }
+    },
+  });
+
+  return stream;
+};
